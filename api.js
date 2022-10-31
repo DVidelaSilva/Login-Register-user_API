3f88d6e6ac1b45e74d600ac8cffbb5ab5b10ebd8
DAVS <41497687+DVidelaSilva@users.noreply.github.com>
2022-10-31 19:56:32 -0300

    Add files via upload

---
+++ api.js
@@ -0,0 +1,34 @@
+const express = require("express") 
+const mongoose = require("mongoose")
+const Animal = require("./animal.controller")
+const { Auth, isAuthenticated } = require("./auth.controller")
+const app = express()
+const port = 3000
+
+
+mongoose.connect('mongodb+srv://DAVS:DAVS@cluster0.nashryt.mongodb.net/miapp?retryWrites=true&w=majority')
+
+app.use(express.json())
+
+app.get("/animals", isAuthenticated, Animal.list)
+app.post("/animals", isAuthenticated, Animal.create)
+app.put("/animals/:id", isAuthenticated, Animal.update)
+app.patch("/animals/:id", isAuthenticated, Animal.update)
+app.delete("/animals/:id", isAuthenticated, Animal.destroy)
+
+app.post("/login", Auth.login)
+app.post("/register", Auth.register)
+
+app.use(express.static("app"))
+
+app.get("/", (req, res) => {
+    res.sendFile(`${__dirname}/index.html`)
+})
+
+app.get("*", (req, res) => {
+    res.status(404).send("Esta pagina no existe")
+})
+
+app.listen(port, () => {
+    console.log("Arrancando la aplicacion")
+})
